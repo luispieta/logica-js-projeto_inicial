3c3cf311442d6b82859806debd961391e4d550cf
luispieta <lufelipepieta@gmail.com>
2025-05-27 22:23:09 -0300

    Melhorias no app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,26 +1,34 @@
-let tentativas = 1;
 let jogo = document.querySelector("div.jogo")
-let numeroAletorio = Math.floor(Math.random() * 100) + 1;
+let comecar = document.querySelector("button.comecar")
+let numeroAletorio = Math.floor(Math.random() * 100) + 1
+let tentativas = 1
+let chutes = []
 
-function vamoComeca(){
+console.log(numeroAletorio)
+console.log(chutes)
+
+comecar.addEventListener("click", function () {
     let mensagemComeco = document.querySelector("div.titulo");
     mensagemComeco.style.display = "none"
     jogo.style.display = "block"
-    comecarJogo()
-}
 
-function comecarJogo() {
-    
+})
+
+document.querySelector("button#verificar").addEventListener("click",function () { // linha para receber só um click
+
     let exibirDiv = document.querySelector("div.container__informacoes")
     let numeroEscolhido = Number(document.querySelector("input#numeroEscolhido").value)
     let resultado = document.querySelector("p#resultado")
+    let numeroChutados = document.querySelector("span#numerosChutados")
     let resultadoTentativas = document.querySelector("p#tentativas")
-    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+    
+    chutes.push(numeroEscolhido)
 
-    console.log(numeroAletorio)
+    numeroChutados.innerHTML = `${chutes}`
 
     if (numeroEscolhido === numeroAletorio) {
-        resultadoTentativas.innerText = `${tentativas} ${palavraTentativa} para acertar o número`;
+        resultadoTentativas.innerText = `${tentativas} ${palavraTentativa} para acertar o número`
         jogo.style.display = "none"
         exibirDiv.style.display = "block"
 
@@ -33,7 +41,7 @@ function comecarJogo() {
                 resultado.innerText = `O número é maior que ${numeroEscolhido}. Tente novamente!`
 
             }
-            tentativas ++;
+            tentativas ++
 
         } else {
             resultado.innerText = "Insira um número de 1 a 100."
@@ -41,9 +49,10 @@ function comecarJogo() {
         } 
     }
 
+    document.querySelector("input#numeroEscolhido").value = ""
 
-}
+})
 
-function retornarJogo(){
-    
-}
+document.querySelector("button.reiniciarJogo").addEventListener("click", function () {
+    location.reload()
+})
